5e3e0a2ca338d1951b2ba42e10a81ac72533d133
Andrea Jemmett <andreajemmett@gmail.com>
2017-01-14 20:33:51 +0100

    moved DOM related functions to PageComponent

--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,26 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { PageComponent } from '../shared';
 
-
-declare let jQuery: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-
-  get innerWidth(): number {
-    return window.innerWidth;
-  }
-
-  get containerWidth(): number {
-    return parseInt(getComputedStyle(jQuery('.container')[0]).width);
-  }
-
-  get leftSpacing(): number {
-    // dunno why the 15 is needed
-    return (innerWidth - this.containerWidth) / 2 + 15;
-  }
-
-}
+export class HomeComponent extends PageComponent { }

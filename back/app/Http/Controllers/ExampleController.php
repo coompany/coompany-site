9383191594ebb57b4f3e7bacc221a396ebcdf32e
Andrea Jemmett <andreajemmett@gmail.com>
2017-01-16 14:14:20 +0100

    init backend

---
+++ back/app/Http/Controllers/ExampleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ExampleController extends Controller
+{
+
+    public function __construct()
+    {
+        //
+    }
+
+    public function protected_action()
+    {
+        return 'OK';
+    }
+
+}

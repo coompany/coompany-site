3bae19eff0dfc7117524147792e52f146d5b85d2
Andrea Jemmett <andreajemmett@gmail.com>
2017-03-05 20:16:30 +0100

    it text, logo & minor changes

--- front/src/app/translate/langs/en.ts
+++ front/src/app/translate/langs/en.ts
@@ -1,49 +1,44 @@
 export const LANG_EN_VALUES = {
+  'navbar.what': 'What we do',
+  'navbar.projects': 'Projects',
+  'navbar.contacts': 'Contacts',
+
   'home.jumbo': `
-    Coompany creates digital products.<br />
-    We offer <b>strategic design</b> and <b>consulting</b> services through our
-    studio in Italy.`,
-  'home.jumbo.btn': 'Contact us',
-  'home.what.title': 'What we do',
-  'home.what.text': `
-    We are an independent digital agency. We create your websites
-    and mobile applications from <b>A to Z</b>: our input includes digital brand
-    strategy, design and web development. Our relentless curiosity for innovation
-    allows us to develop powerful and cutting-edge platforms. Our ‘User Centered’
-    approach makes us different. We consider the ergonomic according to usability
-    criteria in order to make browsing as easy as intuitive and offer an attractive
-    and optimal consultation experience for users.`,
-  'home.projects.title': 'Selected Projects',
+    Progettiamo e realizziamo piattaforme digitali, portiamo i vostri progetti
+    ai confini dello spazio conosciuto e oltre.`,
+  'home.jumbo.btn': 'Contattaci',
+  'home.what.title': 'Cosa facciamo',
+  'home.what.text1': `
+    Non siamo solo un agenzia, ma un laboratorio di sinergie digitali. Creiamo siti
+    internet, applicazioni e piattaforme che connettono persone, idee e aziende.
+    Progettiamo, pensiamo e realizziamo i vostri progetti nello stesso modo nel
+    quale costruiamo i nostri: con una curiosità inarrestabile e un approccio
+    “user-centered”. Crediamo fortemente nel metodo collaborativo e sviluppiamo
+    seguendo questa filosofia.`,
+  'home.what.text2': `
+    Il codice è il nostro mestiere, ma non smettiamo mai
+    di imparare. Realizziamo consulenze strategiche per connettere aziende e brand
+    con i loro clienti, attraverso strumenti digitali. Prepariamo la vostra azienda
+    ad entrare in un mondo di innovazione e di nuove opportunità.
+    Lanciamo i vostri progetti nello spazio infinito nel web e come esperti
+    navigatori gli guidiamo attraverso rotte non ancora tracciate.`,
+  'home.projects.title': 'Progetti',
   'home.projects.more': 'Read more...',
-  'home.clients.title': 'Clients',
-  'home.contacts.title': 'Contacts',
+  'home.clients.title': 'Clienti',
+  'home.contacts.title': 'Contatti',
   'home.contacts.text': `
-    We are an independent digital agency. We create your websites
-    and mobile applications from <b>A to Z</b>: our input includes digital brand
-    strategy, design and web development. Our relentless curiosity for innovation
-    allows us to develop powerful and cutting-edge platforms. Our ‘User Centered’
-    approach makes us different. We consider the ergonomic according to usability
-    criteria in order to make browsing as easy as intuitive and offer an attractive
-    and optimal consultation experience for users.`,
-  'home.contacts.hours': `
-    Mon. to Sat.<br />
-    From: 09:00 To: 18:00<br />
-    Sat. to Sun.<br />
-    Closed`,
-  'home.contacts.theytell': 'They tell about us',
+    Non abbiamo mai smesso di studiare. Per noi la ricerca è un processo infinito, e
+    affrontiamo ogni progetto come una sfida nella quale metterci alla prova al
+    massimo delle nostre capacità. Il modo migliore per capire qual’è il nostro
+    segreto è contattarci: siamo costantemente alla ricerca di nuove sfide.`,
+  'home.contacts.theytell': 'Dicono di noi',
   'home.quotes.1':
     `"Coompany are some of the best developers user interface designers I have met anywhere."
     <br /><br />
    Giovanni Paolin, Project Manager, Cren Design`,
   'home.join.title': 'Join us',
-  'home.join.message.label': 'Your message',
-  'home.join.message.placeholder': 'Your message here...',
-  'home.join.address.label': 'Your e-mail',
-  'home.join.address.placeholder': 'email@example.com',
-  'home.join.send': 'Send',
-  'home.join.sent': 'Sent',
-  'home.join.sending': 'Sending...',
-  'home.join.error': 'Failed...',
+  'home.join.text': 'Lavora insieme a noi sui progetti che sviluppiamo',
+  'home.join.btn': 'Contattaci',
 
   'coo2plan.name': 'Coo2plan',
   'coo2plan.subtitle': 'Web based platform',
@@ -62,25 +57,12 @@ export const LANG_EN_VALUES = {
   'projects.signup.btn.failed': 'Failed...',
   'projects.signup.btn.signed': 'Submitted',
 
-  'footer.sitemap.title': 'Site map:',
-  'footer.findus.title': 'Find us on:',
-  'footer.hear.title': 'We\'d love to hear from you:',
-  'footer.sitemap.home': 'Homepage',
-  'footer.sitemap.what': 'What we do',
-  'footer.sitemap.projects': 'Projects',
-  'footer.sitemap.clients': 'Clients',
-  'footer.sitemap.journal': 'Journal',
-  'footer.sitemap.contacts': 'Contacts',
-  'footer.sitemap.join': 'Join us',
   'footer.text': 'Coompany s.r.l.s. Italy. All rights reserverd.',
 
-  'italy': 'Italy',
-  'btw': 'VAT',
+  'italy': 'Italia',
+  'btw': 'P. IVA',
   'tel': 'Tel',
   'info': 'Info',
-  'assistance': 'Assistance',
-  'venice': 'Venice, Italy',
-  'now': 'Now',
-  'open': 'OPEN',
-  'closed': 'CLOSED'
+  'assistance': 'Assistenza',
+  'venice': 'Vanezia, Italia'
 };
--- front/src/app/translate/langs/it.ts
+++ front/src/app/translate/langs/it.ts
@@ -1,4 +1,68 @@
 export const LANG_IT_VALUES = {
+  'navbar.what': 'Cosa facciamo',
+  'navbar.projects': 'Progetti',
+  'navbar.contacts': 'Contatti',
+
+  'home.jumbo': `
+    Progettiamo e realizziamo piattaforme digitali, portiamo i vostri progetti
+    ai confini dello spazio conosciuto e oltre.`,
+  'home.jumbo.btn': 'Contattaci',
   'home.what.title': 'Cosa facciamo',
-  'home.what.text': 'Questo è quello che facciamo'
+  'home.what.text1': `
+    Non siamo solo un agenzia, ma un laboratorio di sinergie digitali. Creiamo siti
+    internet, applicazioni e piattaforme che connettono persone, idee e aziende.
+    Progettiamo, pensiamo e realizziamo i vostri progetti nello stesso modo nel
+    quale costruiamo i nostri: con una curiosità inarrestabile e un approccio
+    “user-centered”. Crediamo fortemente nel metodo collaborativo e sviluppiamo
+    seguendo questa filosofia.`,
+  'home.what.text2': `
+    Il codice è il nostro mestiere, ma non smettiamo mai
+    di imparare. Realizziamo consulenze strategiche per connettere aziende e brand
+    con i loro clienti, attraverso strumenti digitali. Prepariamo la vostra azienda
+    ad entrare in un mondo di innovazione e di nuove opportunità.
+    Lanciamo i vostri progetti nello spazio infinito nel web e come esperti
+    navigatori gli guidiamo attraverso rotte non ancora tracciate.`,
+  'home.projects.title': 'Progetti',
+  'home.projects.more': 'Read more...',
+  'home.clients.title': 'Clienti',
+  'home.contacts.title': 'Contatti',
+  'home.contacts.text': `
+    Non abbiamo mai smesso di studiare. Per noi la ricerca è un processo infinito, e
+    affrontiamo ogni progetto come una sfida nella quale metterci alla prova al
+    massimo delle nostre capacità. Il modo migliore per capire qual’è il nostro
+    segreto è contattarci: siamo costantemente alla ricerca di nuove sfide.`,
+  'home.contacts.theytell': 'Dicono di noi',
+  'home.quotes.1':
+    `"Coompany are some of the best developers user interface designers I have met anywhere."
+    <br /><br />
+   Giovanni Paolin, Project Manager, Cren Design`,
+  'home.join.title': 'Join us',
+  'home.join.text': 'Lavora insieme a noi sui progetti che sviluppiamo',
+  'home.join.btn': 'Contattaci',
+
+  'coo2plan.name': 'Coo2plan',
+  'coo2plan.subtitle': 'Web based platform',
+  'coo2plan.summary': `We are an independent digital agency.
+    We create your websites and mobile applications from`,
+  'sos.name': 'Scuola Open Source',
+  'sos.subtitle': 'Web based platform',
+  'sos.summary': `We are an independent digital agency.
+    We create your websites and mobile applications from`,
+
+  'projects.signup.title': 'Sign Up',
+  'projects.signup.label': 'Your e-mail',
+  'projects.signup.placeholder': 'example@mail.com',
+  'projects.signup.btn.signup': 'Submit',
+  'projects.signup.btn.signing': 'Submitting...',
+  'projects.signup.btn.failed': 'Failed...',
+  'projects.signup.btn.signed': 'Submitted',
+
+  'footer.text': 'Coompany s.r.l.s. Italy. All rights reserverd.',
+
+  'italy': 'Italia',
+  'btw': 'P. IVA',
+  'tel': 'Tel',
+  'info': 'Info',
+  'assistance': 'Assistenza',
+  'venice': 'Vanezia, Italia'
 };

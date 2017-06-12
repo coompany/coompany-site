39508269947f034d1cb96274ff615a79d76eb3c6
Andrea Jemmett <andreajemmett@gmail.com>
2017-06-12 08:38:11 +0200

    replaced deprecated OpaqueToken

--- front/src/app/translate/translations.ts
+++ front/src/app/translate/translations.ts
@@ -1,8 +1,8 @@
-import { OpaqueToken } from '@angular/core';
+import { InjectionToken } from '@angular/core';
 import { LANG_EN_VALUES } from './langs/en';
 import { LANG_IT_VALUES } from './langs/it';
 
-export const TRANSLATIONS = new OpaqueToken('translations');
+export const TRANSLATIONS = new InjectionToken('translations');
 
 export const dictionary = {
   en: LANG_EN_VALUES,
